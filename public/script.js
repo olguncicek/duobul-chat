e8d5636f069593954c8174c26ca1dc8b8bce493f
olguncicek <115970228+olguncicek@users.noreply.github.com>
2025-12-02 09:48:12 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,27 @@
 const socket = io();
 
+let username = localStorage.getItem("username");
+
+const usernameModal = document.getElementById("usernameModal");
+const usernameInput = document.getElementById("usernameInput");
+const saveUsername = document.getElementById("saveUsername");
+
+if (!username) {
+    usernameModal.style.display = "flex";
+} else {
+    usernameModal.style.display = "none";
+}
+
+saveUsername.addEventListener("click", () => {
+    const name = usernameInput.value.trim();
+    if (name.length < 2) return alert("Kullanıcı adı çok kısa!");
+
+    username = name;
+    localStorage.setItem("username", username);
+
+    usernameModal.style.display = "none";
+});
+
 const input = document.getElementById("msgInput");
 const sendBtn = document.getElementById("sendBtn");
 const messagesUl = document.querySelector(".messages");
@@ -11,23 +33,29 @@ input.addEventListener("keypress", (e) => {
 
 function sendMessage() {
     const msg = input.value.trim();
-    if (!msg) return;
+    if (!msg || !username) return;
 
-    socket.emit("sendMessage", msg);
+    socket.emit("sendMessage", {
+        user: username,
+        text: msg
+    });
+
     input.value = "";
 }
 
-socket.on("newMessage", (msg) => {
-    addMessage(msg);
+socket.on("newMessage", (data) => {
+    addMessage(data);
 });
 
-function addMessage(msg) {
+function addMessage(data) {
     const li = document.createElement("li");
     li.classList.add("message");
+
     li.innerHTML = `
-        <p class="text">${msg}</p>
+        <p class="text"><strong>${data.user}:</strong> ${data.text}</p>
         <span class="date">${new Date().toLocaleTimeString().slice(0,5)}</span>
     `;
+
     messagesUl.appendChild(li);
     messagesUl.scrollTop = messagesUl.scrollHeight;
 }
